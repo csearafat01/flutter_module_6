1ce4ecf778adf0d7a74f643b962b4256cb54d057
csearafat01 <54732088+csearafat01@users.noreply.github.com>
2023-09-19 00:12:13 +0600

    Dynamic ListView Builder with Gesture Detector

--- lib/main.dart
+++ lib/main.dart
@@ -1,215 +1,155 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  // give me some widget
-  runApp(const MyApp());
+  runApp(const PhotoGalleryApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class PhotoGalleryApp extends StatelessWidget {
+  const PhotoGalleryApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: HomeScreen(),
-      title: 'Flutter Module 6',
+    return MaterialApp(
+      title: 'Photo Gallery',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home:  PhotoGalleryHomePage(),
     );
   }
 }
 
-class HomeScreen extends StatelessWidget {
-  const HomeScreen({Key? key}) : super(key: key);
 
-  mySnackBar(message, context){
-    return ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(content: Text(message)
-        )
-    );
-  }
 
-  myAlertDialog(context){
-    return showDialog(
-        context: context,
-        builder: (BuildContext context){
-            return Expanded(
-                child: AlertDialog(
-                  title: Text('Alert!'),
-                  content: Text('Do you want to delete?'),
-                  actions: [
-                    TextButton(onPressed: (){
-                      mySnackBar('Successfully Deleted!', context);Navigator.of(context).pop();
+class PhotoGalleryHomePage extends StatelessWidget {
+  final List<String> imageUrls = [
+    'https://hips.hearstapps.com/hmg-prod/images/cute-cat-photos-1593441022.jpg?crop=0.670xw:1.00xh;0.167xw,0&resize=640:*',
+    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQkN5hV2XmBVX7ckElXZJAYOLKMBm1v1s6qBkrhaxw&s',
+    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSt_zPMis8lyVU9MV0Htg5boyz7NP9LYkEY3A&usqp=CAU',
+    'https://thumbs.dreamstime.com/b/four-cute-cats-20650677.jpg',
+    'https://static.vecteezy.com/system/resources/thumbnails/002/098/204/small/silver-tabby-cat-sitting-on-green-background-free-photo.jpg',
+    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTuwWt86N3mO58gwo4JKldZHI6jOpXTVIMAq8_aIcex&s',
+  ];
 
-                    }, child: Text('yes')),
-                    TextButton(onPressed: (){Navigator.of(context).pop();}, child: Text('No')),
-
-                  ],
-                 )
-              );
-           }
-       );
-  }
-
+  PhotoGalleryHomePage({super.key});
   @override
   Widget build(BuildContext context) {
-    
-    ButtonStyle buttonStyle = ElevatedButton.styleFrom(
-      padding: EdgeInsets.all(25),
-      minimumSize: Size(double.infinity, 60),
-      backgroundColor: Colors.green,
-      foregroundColor: Colors.white,
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.all(Radius.circular(6))
-      )
-    );
     return Scaffold(
-      backgroundColor: Colors.grey.shade200,
       appBar: AppBar(
-
-        title: const Text('Innovation App'),
-        titleSpacing: 10,
-        ///centerTitle: true,
-        toolbarHeight: 60,
-        toolbarOpacity: 1,
-        elevation: 6,
-        backgroundColor: Colors.green,
-        actions: [
-          IconButton(onPressed: (){mySnackBar('comment', context);}, icon: Icon(Icons.comment)),
-          IconButton(onPressed: (){mySnackBar('search', context);}, icon: Icon(Icons.search)),
-          IconButton(onPressed: (){mySnackBar('settings', context);}, icon: Icon(Icons.settings)),
-          IconButton(onPressed: (){mySnackBar('email', context);}, icon: Icon(Icons.email))
-        ],
-
+        title: Center(child: Text('Photo Gallery')),
       ),
-      floatingActionButton: FloatingActionButton(
-        elevation: 18,
-        child: Icon(Icons.add),
-        backgroundColor: Colors.green,
-        onPressed: (){mySnackBar('FloatingAction', context);},
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: 1,
-        items: [
-          BottomNavigationBarItem(icon: Icon(Icons.message), label: 'Message'),
-          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
-          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profile'),
-        ],
-        onTap: (int index){
-          if(index == 0){
-            mySnackBar('message', context);
-          }if(index == 1){
-            mySnackBar('home', context);
-          }if(index == 2){
-            mySnackBar('profile', context);
-          }
-        },
-      ),
-      drawer: Drawer(
-        child: ListView(
+      body: SingleChildScrollView(
+        padding: EdgeInsets.all(8),
+        child: Column(
           children: [
-            DrawerHeader(
-                padding: EdgeInsets.all(0),
-                child: UserAccountsDrawerHeader(
-                  decoration: BoxDecoration(color: Colors.white70),
-                  accountName: Text('Md. Arafat Rahman',style: TextStyle(color: Colors.black)),
-                  accountEmail: Text('arafat.roman@outlook.com', style: TextStyle(color: Colors.black)),
-                  currentAccountPicture: Image.network("https://cdn-images-1.medium.com/v2/resize:fit:1200/1*5-aoK8IBmXve5whBQM90GA.png"),
-                  onDetailsPressed: (){
-                    mySnackBar('My Profile', context);
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Text(
+                'Welcome to My Photo Gallery!',
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: TextField(
+                decoration: InputDecoration(
+                  hintText: 'Search for photos...',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(8.0),
+                  ),
+                ),
+              ),
+            ),
+            GridView.builder(
+              shrinkWrap: true,
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 3,
+                crossAxisSpacing: 8,
+                mainAxisSpacing: 8,
+              ),
+              itemCount: imageUrls.length,
+              itemBuilder: (context, index) {
+                return GestureDetector(
+                  onTap: () {
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(
+                        content: Text('Photo ${index + 1} Clicked!'),
+                      ),
+                    );
                   },
-                )),
-            ListTile(
-              leading: Icon(Icons.home),
-              title: Text('Home'),
-              onTap: (){
-                mySnackBar('home', context);
+                  child: Card(
+                    elevation: 6,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: [
+                        Image.network(
+                          imageUrls[index],
+                          height: 84,
+                          fit: BoxFit.cover,
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Center(child: Text('Photo ${index + 1}')),
+                        ),
+                      ],
+                    ),
+                  ),
+                );
               },
             ),
             ListTile(
-              leading: Icon(Icons.email),
-              title: Text('Email'),
-              onTap: (){
-                mySnackBar('Email', context);
-              },
+              leading: Image.network(
+                'https://hips.hearstapps.com/hmg-prod/images/cute-cat-photos-1593441022.jpg?crop=0.670xw:1.00xh;0.167xw,0&resize=640:*',
+                height: 100,
+                width: 80,
+                fit: BoxFit.cover,
+              ),
+              title: Text('Photo 1'),
+              subtitle: Text('Description for Photo 1'),
             ),
+
             ListTile(
-              leading: Icon(Icons.phone),
-              title: Text('Phone'),
-              onTap: (){
-                mySnackBar('Phone', context);
-              },
+              leading: Image.network(
+                'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQkN5hV2XmBVX7ckElXZJAYOLKMBm1v1s6qBkrhaxw&s',
+                height: 100,
+                width: 80,
+                fit: BoxFit.cover,
+              ),
+              title: Text('Photo 2'),
+              subtitle: Text('Description for Photo 2'),
+            ),
+
+            ListTile(
+              leading: Image.network(
+                'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSt_zPMis8lyVU9MV0Htg5boyz7NP9LYkEY3A&usqp=CAU',
+                height: 80,
+                width: 80,
+                fit: BoxFit.cover,
+              ),
+              title: Text('Photo 3'),
+              subtitle: Text('Description for Photo 3'),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: FloatingActionButton(
+                elevation: 18,
+                child: Icon(Icons.upload),
+                onPressed: () {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(
+                      content: Text('Photos Uploaded Successfully!'),
+                    ),
+                  );
+                },
+                //child: Icon(Icons.upload),
+              ),
             ),
           ],
         ),
       ),
-      endDrawer: Drawer(
-        child: ListView(
-          children: [
-            DrawerHeader(
-                padding: EdgeInsets.all(0),
-                child: UserAccountsDrawerHeader(
-                  decoration: BoxDecoration(color: Colors.white70),
-                  accountName: Text('Md. Arafat Rahman',style: TextStyle(color: Colors.black)),
-                  accountEmail: Text('arafat.roman@outlook.com', style: TextStyle(color: Colors.black)),
-                  currentAccountPicture: Image.network("https://cdn-images-1.medium.com/v2/resize:fit:1200/1*5-aoK8IBmXve5whBQM90GA.png"),
-                  onDetailsPressed: (){
-                    mySnackBar('My Profile', context);
-                  },
-                )),
-            ListTile(
-              leading: Icon(Icons.home),
-              title: Text('Home'),
-              onTap: (){
-                mySnackBar('home', context);
-              },
-            ),
-            ListTile(
-              leading: Icon(Icons.email),
-              title: Text('Email'),
-              onTap: (){
-                mySnackBar('Email', context);
-              },
-            ),
-            ListTile(
-              leading: Icon(Icons.phone),
-              title: Text('Phone'),
-              onTap: (){
-                mySnackBar('Phone', context);
-              },
-            ),
-          ],
-        ),
-      ),
-      body: (
-      Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child:
-            TextField(decoration: InputDecoration(border: OutlineInputBorder(),labelText: 'First Name'),),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child:
-            TextField(decoration: InputDecoration(border: OutlineInputBorder(),labelText: 'Last Name'),),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child:
-            TextField(decoration: InputDecoration(border: OutlineInputBorder(),labelText: 'Email Address'),),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child:
-            ElevatedButton(onPressed: (){},child: Text('Submit'),style: buttonStyle,),
-          ),
-
-        ],
-      )
-      ),
-
     );
   }
 }
